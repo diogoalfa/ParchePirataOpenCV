fb57582131b6912c1be93fae8fa90dc6b79f0b9d
diogoalfa <Diogo@client-186-11-229-219.imovil.entelpcs.cl>
2013-11-12 17:51:28 -0300

    Initial Commit

---
+++ ParchePirataOpenCV/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ParchePirataOpenCV
+//
+//  Created by Diego Cristobal Navia Fuentes on 12-11-13.
+//  Copyright (c) 2013 Diego Cristobal Navia Fuentes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
